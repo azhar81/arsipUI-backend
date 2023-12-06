4cd7e751594cfcd4447470c3554b57f16689c7db
azhar81 <a.hassanuddin02@gmail.com>
2023-12-06 16:41:56 +0700

    setting up environment variables

--- tugas_akhir/settings.py
+++ tugas_akhir/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import environ
 from pathlib import Path
+
+# Setting up environ for environment variables
+env = environ.Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bicmo7f3_+l%x3cg=$rjp@ev2jh5(qv3_nf!scc_t+#g3dlfej"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -56,7 +61,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    env('FRONTEND_URL'),
 ]
 
 ROOT_URLCONF = "tugas_akhir.urls"
