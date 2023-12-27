19304d8459d6c1a630d6c5a1354a44fadeb6e36d
azhar81 <a.hassanuddin02@gmail.com>
2023-12-27 10:42:08 +0700

    admin crud

--- users/models.py
+++ users/models.py
@@ -3,8 +3,15 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
+    CONTRIBUTOR = "contributor"
+    VERIFICATOR = "verificator"
+
+    USERTYPE_CHOICES = [
+        (CONTRIBUTOR, "Contributor"),
+        (VERIFICATOR, "Verificator"),
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_type = models.CharField(max_length=20)
+    user_type = models.CharField(max_length=11, choices=USERTYPE_CHOICES)
 
     def __str__(self):
         return self.user.username
